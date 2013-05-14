a27122541e1faacf51cefa925f57cc4a8406ff06
Sam Decrock <sam.decrock@gmail.com>
2013-05-14 16:56:31 +0200

    Twittersearch module toegevoegd

---
+++ twitter.config.js
@@ -0,0 +1,11 @@
+/*
+ * Insert Twitter DEV keys in this file
+ * For use of multiple Twitterhoses, u can create Twitter apps with multiple users.
+ */
+
+exports.twitter = {
+	consumerKey: 	"",
+	consumerSecret: "",
+	token: 			"",
+	secret: 		""
+}
